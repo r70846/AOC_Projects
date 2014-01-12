cead4af7ede43afeb8f66224116cbfd4930dd4f4
Russell Gaspard <russg@fullsail.edu>
2014-01-11 19:40:49 -0500

    first commit

---
+++ rgaspardOne/rgaspardOne/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  rgaspardOne
+//
+//  Created by Russell Gaspard on 1/11/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
