bfef2d608e030e635b0dad397a447eaa3e2d3068
Russell Gaspard <russg@fullsail.edu>
2014-01-23 10:41:02 -0500

    basic UI added

---
+++ rgaspardThree/baseProject/EventView.h
@@ -0,0 +1,13 @@
+//
+//  EventView.h
+//  rgaspardThree
+//
+//  Created by Russell Gaspard on 1/23/14.
+//  Copyright (c) 2014 FullSail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventView : UIViewController
+
+@end
---
+++ rgaspardThree/baseProject/EventView.m
@@ -0,0 +1,38 @@
+//
+//  EventView.m
+//  rgaspardThree
+//
+//  Created by Russell Gaspard on 1/23/14.
+//  Copyright (c) 2014 FullSail. All rights reserved.
+//
+
+#import "EventView.h"
+
+@interface EventView ()
+
+@end
+
+@implementation EventView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
