51d32dfa8e18003e43e4809979e9fdbc9c4b46e2
Russell Gaspard <russg@fullsail.edu>
2014-01-30 20:46:42 -0500

    new files for project4 added

---
+++ rgaspardFour/rgaspardFour/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rgaspardFour
+//
+//  Created by Russell Gaspard on 1/30/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rgaspardFour/rgaspardFour/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rgaspardFour
+//
+//  Created by Russell Gaspard on 1/30/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
